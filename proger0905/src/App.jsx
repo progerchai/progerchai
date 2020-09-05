21a3ffa4a8855a94f11acc600dba6d5de2910d2a
proger <progerchai@qq.com>
2020-09-06 01:27:02 +0800

    feat:add map

---
+++ proger0905/src/App.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import styles from "./App.less";
+import { AMapScene, Marker } from "@antv/l7-react";
+function App() {
+  return (
+    <>
+      <AMapScene
+        map={{
+          center: [120.190683, 30.260876],
+          pitch: 0,
+          style: "light",
+          zoom: 17,
+        }}
+        style={{
+          position: "absolute",
+          top: 0,
+          left: 0,
+          right: 0,
+          bottom: 0,
+        }}
+      >
+        <Marker
+          option={{
+            color: "red",
+          }}
+          anchor="bottom"
+          lnglat={[120.190683, 30.260876]}
+        >
+          <div className={styles.focusPoint}>123</div>
+        </Marker>
+      </AMapScene>
+    </>
+  );
+}
+
+export default App;
