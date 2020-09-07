ce26c3cb5aeac6d08077e217d3ad00f78ef10197
proger <progerchai@qq.com>
2020-09-07 22:54:03 +0800

    getLocation Demo

--- proger0905/src/App.jsx
+++ proger0905/src/App.jsx
@@ -1,7 +1,41 @@
-import React from "react";
+import React, { useEffect } from "react";
 import styles from "./App.less";
 import { AMapScene, Marker } from "@antv/l7-react";
+// import RNLocation from "react-native-location";
 function App() {
+  useEffect(() => {
+    // RNLocation.configure({
+    //   distanceFilter: 5.0,
+    // });
+    // RNLocation.requestPermission({
+    //   ios: "whenInUse",
+    //   android: {
+    //     detail: "coarse",
+    //   },
+    // }).then((granted) => {
+    //   if (granted) {
+    //     console.log(1111, granted);
+    //     this.locationSubscription = RNLocation.subscribeToLocationUpdates(
+    //       (locations) => {
+    //         /* Example location returned
+    //         {
+    //           speed: -1,
+    //           longitude: -0.1337,
+    //           latitude: 51.50998,
+    //           accuracy: 5,
+    //           heading: -1,
+    //           altitude: 0,
+    //           altitudeAccuracy: -1
+    //           floor: 0
+    //           timestamp: 1446007304457.029,
+    //           fromMockProvider: false
+    //         }
+    //         */
+    //       }
+    //     );
+    //   }
+    // });
+  });
   return (
     <>
       <AMapScene
@@ -26,7 +60,7 @@ function App() {
           anchor="bottom"
           lnglat={[120.190683, 30.260876]}
         >
-          <div className={`test ${styles.focusPoint}`}>123</div>
+          <div className={styles.focusPoint}></div>
         </Marker>
       </AMapScene>
     </>
