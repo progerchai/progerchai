e107d246fc12b75d536a10c9e6d6538d2c5e42a8
proger <progerchai@qq.com>
2020-12-02 01:28:46 +0800

    dva test add async effect

--- proger1130/dva-code-learning/src/index.jsx
+++ proger1130/dva-code-learning/src/index.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import dva, { connect } from "./dva";
+import dva, { connect } from "dva";
 import { createBrowserHistory } from "history";
 
 let app = dva({
@@ -19,9 +19,18 @@ app.model({
     add(state) {
       return { number: state.number + 2 };
     },
-    ["counter/add"](state) {
-      // ["counter/add"] 这样写的原因是,getReducer还没有对 type 做处理
-      return { number: state.number + 1 };
+    minus(state) {
+      return { number: state.number - 1 };
+    },
+    // ["counter/add"](state) {
+    //   // ["counter/add"] 这样写的原因是,getReducer还没有对 type 做处理
+    //   return { number: state.number + 1 };
+    // },
+  },
+  effects: {
+    *asyncAdd(state, { put }) {
+      yield delay(1000);
+      yield put({ type: "add" });
     },
   },
   subscriptions: {},
@@ -31,7 +40,13 @@ function Counter(props) {
   return (
     <div>
       <div> {props.number} </div>
-      <button onClick={() => props.dispatch({ type: "add" })}>+</button>
+      <button onClick={() => props.dispatch({ type: "counter/add" })}>+</button>
+      <button onClick={() => props.dispatch({ type: "counter/minus" })}>
+        -
+      </button>
+      <button onClick={() => props.dispatch({ type: "counter/asyncAdd" })}>
+        asyncAdd
+      </button>
     </div>
   );
 }
@@ -40,3 +55,12 @@ let ConnectCounter = connect((state) => state.counter)(Counter);
 
 app.router(() => <ConnectCounter />);
 app.start("#root");
+
+// 自定义延迟函数
+function delay(time) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve();
+    }, time);
+  });
+}
