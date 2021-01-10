e2a8f2ecc0d0586340733db91a848ea6320db89f
proger <progerchai@qq.com>
2021-01-10 15:56:35 +0800

    react-native ios加载步骤description

--- proger200109/proger/ios/proger/AppDelegate.m
+++ proger200109/proger/ios/proger/AppDelegate.m
@@ -30,18 +30,21 @@ static void InitializeFlipper(UIApplication *application) {
 #ifdef FB_SONARKIT_ENABLED
   InitializeFlipper(application);
 #endif
-
+  // 1. 获取桥接文件
   RCTBridge *bridge = [[RCTBridge alloc] initWithDelegate:self launchOptions:launchOptions];
+  // 2. 根据模块去加载根视图
   RCTRootView *rootView = [[RCTRootView alloc] initWithBridge:bridge
                                                    moduleName:@"proger"
                                             initialProperties:nil];
 
   rootView.backgroundColor = [[UIColor alloc] initWithRed:1.0f green:1.0f blue:1.0f alpha:1];
-
+  // 3. 设计窗口
   self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
+  // 4. 设置窗口的根控制器
   UIViewController *rootViewController = [UIViewController new];
   rootViewController.view = rootView;
   self.window.rootViewController = rootViewController;
+  // 5. 让窗口显示出来
   [self.window makeKeyAndVisible];
   return YES;
 }
