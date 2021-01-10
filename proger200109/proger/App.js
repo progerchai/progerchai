1f8ebcf7e0e7c4294e834fc3e1e1091d007fc5e7
proger <progerchai@qq.com>
2021-01-10 18:10:36 +0800

    es-lint configurations

--- proger200109/proger/App.js
+++ proger200109/proger/App.js
@@ -10,29 +10,38 @@ import React from 'react';
 import {
   SafeAreaView,
   StyleSheet,
-  ScrollView,
+  // ScrollView,
   View,
   Text,
   StatusBar,
 } from 'react-native';
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+// import {
+//   Header,
+//   LearnMoreLinks,
+//   Colors,
+//   DebugInstructions,
+//   ReloadInstructions,
+// } from 'react-native/Libraries/NewAppScreen';
 
 const App: () => React$Node = () => {
   return (
     <>
       <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <Text>RN app</Text>
+      <SafeAreaView style={{ flex: 1 }}>
+        <View style={styles.mainViewStyle}>
+          <Text>RN app</Text>
+        </View>
       </SafeAreaView>
     </>
   );
 };
+const styles = StyleSheet.create({
+  mainViewStyle: {
+    width: 200,
+    height: 200,
+    backgroundColor: 'blue',
+  },
+});
 
 export default App;
