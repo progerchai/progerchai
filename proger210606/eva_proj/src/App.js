140cb75ab47f8ff472358e00dbedc81748200512
proger <progerchai@qq.com>
2021-06-06 23:34:40 +0800

    evajs

---
+++ proger210606/eva_proj/src/App.js
@@ -0,0 +1,15 @@
+import './App.css'
+import './resource'
+
+function App() {
+  return (
+    <div className="App">
+      <span>画布区域:</span>
+      <div className={'canvas'}>
+        <canvas id="canvas"></canvas>
+      </div>
+    </div>
+  )
+}
+
+export default App
